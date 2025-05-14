b9852f9d189d00968c0bc51a34bbbb25febbc996
Angela-Sin <anzela.sinia@gmail.com>
2025-05-14 14:44:42 +0100

    fixed add to back button loop

--- bag/views.py
+++ bag/views.py
@@ -40,13 +40,17 @@ def add_to_bag(request, item_id):
     
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+
+    product = get_object_or_404(Product, pk=item_id)
     
     bag = request.session.get('bag', {})
 
     if item_id in bag:
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}.')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag.')
 
     request.session['bag'] = bag
     
