77527cc4a096297f8de0b8dd7e98b774c4e51c80
Angela-Sin <anzela.sinia@gmail.com>
2025-06-09 16:57:19 +0100

    add metaclass to day spell module

--- day_spell/models.py
+++ day_spell/models.py
@@ -9,5 +9,8 @@ class DaySpell(models.Model):
     moon_phase = models.CharField(max_length=50, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
 
+    class Meta:
+        db_table = "dayspell"
+
     def __str__(self):
         return f"{self.title} ({self.keyword})"
